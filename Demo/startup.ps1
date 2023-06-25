95256f77f18dccc3112512ca6b6fd15f79053bf8
emrys <emrys@macinally.de>
2023-06-25 13:52:17 +0200

    first commit

---
+++ Demo/startup.ps1
@@ -0,0 +1,10 @@
+﻿# add anything that should be done during first startup
+
+#configure git
+git config --global --add safe.directory "C:/Users/$env:USERNAME/workspace/"
+git config --global --add safe.directory "C:/Users/$env:USERNAME/workspace" # trust workspace
+git config --global http.sslBackend schannel # use windows certificate store
+git config --global user.name emrys
+git config --global user.email emrys@macinally.de
+git config --global credential.credentialStore dpapi
+git config --global credential.https://github.com.username macces
